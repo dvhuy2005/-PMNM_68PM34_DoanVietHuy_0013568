602ce193e5aff6f3194f7acde5707c887a42567f
Huy Đoàn <viethuydoan06@gmail.com>
2026-06-04 01:07:39 +0700

    Commit 4: Controller.php + ConnectDB, add sinhvienModel::getAll(), Hiển thị danh sách sinh viên

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,11 +1,25 @@
 <?php
-class sinhvien{
-    public function index(){
-        echo "Đây là trang tạo sinh viên";
+require_once '../app/core/Controller.php';
+
+class sinhvien extends Controller {
+
+    public function index()
+    {
+        // 1. Gọi model 'sinhvienModel' dậy làm việc
+        $sinhvienModel = $this->model('sinhvienModel');
+
+        // 2. Chạy hàm lấy 22 sinh viên mồi dưới Database lên
+        $sinhviens = $sinhvienModel->getAllSinhVien();
+
+        // 3. Bắn cục dữ liệu sang file giao diện HTML/CSS để hiển thị
+        $this->view('sinhvien/index', [
+            'sinhviens' => $sinhviens
+        ]);
     }
-    public function create(){
-        echo "Đây là trang tạo sinh viên";
+
+    public function create()
+    {
+        require_once '../app/views/sinhvien/create.php';
     }
 }
-
 ?>
