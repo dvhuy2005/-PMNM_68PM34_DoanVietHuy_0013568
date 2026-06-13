0bb52b2d18f07dd875568d4d8ab1f872867c740f
Huy Đoàn <viethuydoan06@gmail.com>
2026-06-13 11:40:20 +0700

    Commit 5: Masterlayout + header, footer (patial)

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -5,15 +5,17 @@ class sinhvien extends Controller {
 
     public function index()
     {
-        // 1. Gọi model 'sinhvienModel' dậy làm việc
+        // 1. Gọi model 'sinhvienModel' làm việc
         $sinhvienModel = $this->model('sinhvienModel');
 
         // 2. Chạy hàm lấy 22 sinh viên mồi dưới Database lên
         $sinhviens = $sinhvienModel->getAllSinhVien();
 
         // 3. Bắn cục dữ liệu sang file giao diện HTML/CSS để hiển thị
-        $this->view('sinhvien/index', [
-            'sinhviens' => $sinhviens
+        $this->view('layout/masterlayout', [
+            'title'     => 'Trang Danh Sách Sinh Viên', // Tiêu đề tab web
+            'viewname'  => 'sinhvien/index',           // Tên file con để nhúng vào giữa (chính là file app/views/sinhvien/index.php)
+            'sinhviens' => $sinhviens                 // Truyền dữ liệu để file con bốc ra dùng
         ]);
     }
 
